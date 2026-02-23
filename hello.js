1b58a2d5fef8df0e70eba2ee87a896da87d9e442
aTamQuietCode <a.tamba.work@outlook.jp>
2026-02-23 14:44:44 +0900

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,7 @@
+// NZ移住への第一歩
+const goal = "New Zealand";
+const languages = ["TypeScript", "React", "English"];
+
+console.log(`Target: ${goal}`);
+console.log(`Learning: ${languages.join(", ")}`);
+console.log("Status: Initializing migration sequence...");
