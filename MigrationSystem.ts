810c3a583b53a0d3b6eee55702f9bae739fb44a0
aTamQuietCode <a.tamba.work@outlook.jp>
2026-03-01 21:40:29 +0900

    Add MigrationSystem.ts

---
+++ MigrationSystem.ts
@@ -0,0 +1,31 @@
+// 1. 型の定義（インターフェース）
+interface Citizen {
+    name: string;
+    status: 'Ready' | 'Searching' | 'Migrated';
+    assets: number; // NZD
+}
+
+// 2. 基本クラス
+class Migrator implements Citizen {
+    constructor(
+        public name: string,
+        public status: 'Ready' | 'Searching' | 'Migrated',
+        public assets: number
+    )
+    {}
+
+    // 資産状況を表示するメソッド
+    displayStatus(): void {
+        console.log(`--- ${this.name}'s Status ---`);
+        console.log(`Target: New Zealand 🇳🇿`);
+        console.log(`Current Assets: ${this.assets.toLocaleString()} NZD 🛡️`);
+
+        if(this.assets >= 5000) {
+            console.log("Defense: shield Level MAX");
+        }        
+    }
+}
+
+// 3. インスタンス化と実行
+const me =  new Migrator(`aTamQuiet_Code`, `Ready`, 5400);
+me.displayStatus();
